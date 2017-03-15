769bd96204bfbbc06a2596e0272faa17ef55cc9d
David Huerta <d.huerta@carfax.eu>
2017-03-15 12:46:20 +0100

    correct operation order

--- src/AppBundle/Controller/PresupuestoController.php
+++ src/AppBundle/Controller/PresupuestoController.php
@@ -379,7 +379,7 @@ class PresupuestoController extends Controller
     //$presupuesto_importe_total = $presupuesto_importe_base + $presupuesto_importe_iva;
 
     $presupuesto_importe_total = $presupuesto->getImporte();
-    $presupuesto_importe_base =  $presupuesto_importe_total / 1 + (self::$IVA / 100);
+    $presupuesto_importe_base =  $presupuesto_importe_total / (1 + (self::$IVA / 100));
     $presupuesto_importe_iva = ($presupuesto_importe_base * self::$IVA) / 100;
 
     $current_page = 1;
